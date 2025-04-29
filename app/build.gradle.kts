f88404b0dfd48a0ee66d4d414a42be781b5f99c4
AustinCarlile <akc0697@gmail.com>
2025-04-28 21:03:07 -0700

    Changed minSdk to 34, so it can run on my phone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.carlile_finalprojectpart1"
-        minSdk = 35
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
